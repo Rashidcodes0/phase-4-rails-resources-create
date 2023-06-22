2f027f9d95ba1b0833b5c0e9b66ec740668f840e
Abdirashidnoor <abdirashid16112@gmail.com>
2023-06-22 11:04:59 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+    def create
+        bird = Bird.create(name: params[:name], species: params[:species])
+        render json: bird, status: :created
+      end
 end
